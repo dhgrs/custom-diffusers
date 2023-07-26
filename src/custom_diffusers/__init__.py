38a5e6e31386b7ae49a7056e973ddd95c4f9c935
dhgrs <d.higurashi@gmail.com>
2023-07-26 16:20:59 +0000

    Add legacy support

--- src/custom_diffusers/__init__.py
+++ src/custom_diffusers/__init__.py
@@ -1,10 +1,23 @@
-from .controlnet import (
-    ControlNet2dModel,
-    ControlNetOutput,
-    UNet2dModel,
-    UNet2dModelArgs,
-    UNet2DOutput,
-)
+import diffusers
+import packaging
+
+if packaging.version.parse(diffusers.__version__) < packaging.version.parse("0.18.0"):
+    from .legacy_controlnet import (
+        ControlNet2dModel,
+        ControlNetOutput,
+        UNet2dModel,
+        UNet2dModelArgs,
+        UNet2DOutput,
+    )
+else:
+    from .controlnet import (
+        ControlNet2dModel,
+        ControlNetOutput,
+        UNet2dModel,
+        UNet2dModelArgs,
+        UNet2DOutput,
+    )
+
 
 __all__ = [
     "ControlNet2dModel",
